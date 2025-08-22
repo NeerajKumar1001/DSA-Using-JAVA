a83563ce2ce6639473e5424f5ecc8a3f6f900dd3
Neeraj Kumar <144528779+NeerajKumar1001@users.noreply.github.com>
2025-08-22 15:18:44 +0530

    trappedwater

--- array1.java
+++ array1.java
@@ -147,8 +147,61 @@ public static int binarySearch(int numbers[] , int key){
             System.out.println();
         }
     }
+
+
+//max subarray sum (brute force)
+
+    public static void maxSubarraySum(int numbers[]){
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for(int i = 0; i < numbers.length; i++){
+            int start = i;
+            for(int j = i; j < numbers.length; j++){
+               int end = j;
+               currSum = 0;
+               for(int k = start ; k<= end; k++){
+                currSum += numbers[k];
+               }
+               System.out.println(currSum);
+               if(maxSum < currSum){
+                maxSum = currSum;
+               }
+            }
+        }
+        System.out.println("max sum = " + maxSum);
+    }
+
+
+//max subarray sun (prefix sum)    
+    public static void maxSubarraySumPrefix(int numbers[]){
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[numbers.length];
+
+        //calculate prefix array
+        prefix[0] = numbers[0];
+        for(int i = 1; i < prefix.length; i++){
+            prefix[i] = prefix[i-1] + numbers[i];
+        }
+
+        for(int i = 0; i < numbers.length; i++){
+            int start = i;
+            for(int j = i; j < numbers.length; j++){
+               int end = j;
+               currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start-1];
+
+
+               System.out.println(currSum);
+               if(maxSum < currSum){
+                maxSum = currSum;
+               }
+            }
+        }
+        System.out.println("max sum = " + maxSum);
+    }
     public static void main(String[] args) {
         int numbers[] = {2,4,6,8,10};
-        printSubArrays(numbers);
+        maxSubarraySumPrefix(numbers);
     }
-}
+} 
